970c6c146b26e7430d106fdc6434535ae87a7dcb
omi-72 <jannat.omi722@gmail.com>
2022-06-10 23:45:25 +0600

    @Columinfo(name= title, details, priority on String name)

--- app/src/main/java/com/example/notescit/db/Note.java
+++ app/src/main/java/com/example/notescit/db/Note.java
@@ -1,16 +1,19 @@
 package com.example.notescit.db;
 
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 
 @Entity
 public class Note {
 
-
+    @ColumnInfo(name = "title")
     String noteTitle;
 
+    @ColumnInfo(name = "details")
     String noteDetails;
 
+    @ColumnInfo(name = "priority")
     String notePriority;
 
 }
