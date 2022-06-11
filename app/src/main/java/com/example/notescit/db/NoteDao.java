83fecd7c9cfe9d0d54a4e5078014982631bd2d96
omi-72 <jannat.omi722@gmail.com>
2022-06-11 23:49:50 +0600

    on noteDao we insertNote

--- app/src/main/java/com/example/notescit/db/NoteDao.java
+++ app/src/main/java/com/example/notescit/db/NoteDao.java
@@ -1,4 +1,12 @@
 package com.example.notescit.db;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+
+@Dao
 public interface NoteDao {
+
+    @Insert
+    public void insertNote(Note note);
+
 }
