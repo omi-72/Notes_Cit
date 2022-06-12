605a319124b4a805e38268a6348fa1c2292a9014
omi-72 <jannat.omi722@gmail.com>
2022-06-12 22:49:19 +0600

    on NoteDatabase add Database , entity, version & exportschema false

--- app/src/main/java/com/example/notescit/db/NoteDatabase.java
+++ app/src/main/java/com/example/notescit/db/NoteDatabase.java
@@ -1,7 +1,9 @@
 package com.example.notescit.db;
 
+import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {Note.class}, version = 1, exportSchema = false)
 public abstract class NoteDatabase extends RoomDatabase {
 
     public abstract NoteDao getDao();
