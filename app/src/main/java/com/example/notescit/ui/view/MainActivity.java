dec16b9097ce87c10f521e0863a365e30e9a1ff7
omi-72 <jannat.omi722@gmail.com>
2022-06-15 23:31:37 +0600

    findviewByid on fab_addNote

--- app/src/main/java/com/example/notescit/ui/view/MainActivity.java
+++ app/src/main/java/com/example/notescit/ui/view/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
        NoteDatabase.getInstance(this).getDao().insertNote(note);
 
-       
+       fab_addNote= findViewById(R.id.add_note);
 
     }
 }
