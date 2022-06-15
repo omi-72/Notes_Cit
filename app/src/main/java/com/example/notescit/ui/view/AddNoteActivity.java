efb6372fd752a5c64cdf5d3eafb6b92359418b7d
omi-72 <jannat.omi722@gmail.com>
2022-06-15 23:24:44 +0600

    create empty name as AddNoteActivity activity on view package & manifest add AddNoteActivity

---
+++ app/src/main/java/com/example/notescit/ui/view/AddNoteActivity.java
@@ -0,0 +1,16 @@
+package com.example.notescit.ui.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.notescit.R;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+    }
+}
