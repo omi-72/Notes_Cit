e3a4c6d582a9b71e78ba841c0470ae5d40aa681a
omi-72 <jannat.omi722@gmail.com>
2022-06-16 19:09:38 +0600

    give find view By id on Add note xml edt id

--- app/src/main/java/com/example/notescit/ui/view/AddNoteActivity.java
+++ app/src/main/java/com/example/notescit/ui/view/AddNoteActivity.java
@@ -3,14 +3,30 @@ package com.example.notescit.ui.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 import com.example.notescit.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AddNoteActivity extends AppCompatActivity {
+
+    AutoCompleteTextView edt_priority;
+    TextInputEditText edt_title, edt_details;
+    Button btn_addNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
+        edt_title= findViewById(R.id.edt_title);
+        edt_details= findViewById(R.id.edt_details);
+        edt_priority= findViewById(R.id.edt_priority);
+        btn_addNote= findViewById(R.id.btn_addNote);
+
+
+
+
     }
 }
