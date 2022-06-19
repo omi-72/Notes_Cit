46ff52b59c2d9422089b5590ef50cff7b0e8e9a4
omi-72 <jannat.omi722@gmail.com>
2022-06-19 23:04:36 +0600

    for listNote Query SELECT *

--- app/src/main/java/com/example/notescit/db/NoteDao.java
+++ app/src/main/java/com/example/notescit/db/NoteDao.java
@@ -3,6 +3,7 @@ package com.example.notescit.db;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
 import androidx.room.Update;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public interface NoteDao {
     @Delete
     void  deleteNote(Note note);
 
+    @Query("SELECT * FROM Note")
     List<Note> getNotes();
 
 }
