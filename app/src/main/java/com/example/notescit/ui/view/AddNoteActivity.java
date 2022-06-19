64a4f4940e50a2405e2026f1c3c4351d797dc4d2
omi-72 <jannat.omi722@gmail.com>
2022-06-19 22:53:28 +0600

    edt_priority.setTag(priority.equals()?mPriority:priority)

--- app/src/main/java/com/example/notescit/ui/view/AddNoteActivity.java
+++ app/src/main/java/com/example/notescit/ui/view/AddNoteActivity.java
@@ -38,6 +38,7 @@ public class AddNoteActivity extends AppCompatActivity {
             String title = edt_title.getText().toString().trim();
             String details = edt_details.getText().toString().trim();
             String priority = edt_priority.getText().toString().trim();
+            edt_priority.setTag(priority.equals("") ? mPriority : priority);
 
             if (title.equals("") || details.equals("")) {
                 Toast.makeText(this, "All Fields Are Required", Toast.LENGTH_SHORT).show();
